d0094a61d06e7d6d5731e41d5a3571114327692a
dimitarhristov86 <dimitar_hristov86@abv.bg>
2022-04-12 20:14:39 +0300

    initial commit

---
+++ shop/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created', 'updated']
+    list_filter = ['name', 'created', 'updated']
+    prepopulated_fields = {'slug': ('name',)}
