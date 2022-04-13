cf8d49866b166cdb73cb2c6308b7879daa0fed5f
dimitarhristov86 <dimitar_hristov86@abv.bg>
2022-04-13 22:46:48 +0300

    add contact-form + send_mail functionality

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.mail import send_mail, get_connection
+from django.http import HttpResponseRedirect
+from .forms import ContactForm
 from .models import Category, Product
 
 
@@ -18,3 +21,24 @@ def product_detail(request, id, slug):
     return render(request, 'shop/product/detail.html', {'product': product})
 
 
+def contact(request):
+    submitted = False
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = 'Question'
+            body = {
+                'full_name': form.cleaned_data['full_name'],
+                'subject': form.cleaned_data['subject'],
+                'email_address': form.cleaned_data['email_address'],
+                'message': form.cleaned_data['message'],
+            }
+            message = "\n".join(body.values())
+            conn = get_connection('django.core.mail.backends.smtp.EmailBackend')
+            send_mail(subject, message, body['email_address'], ['emiliyawoodart@gmail.com'], fail_silently=False, connection=conn)
+            return HttpResponseRedirect('/contact?submitted=True')
+    else:
+        form = ContactForm()
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'shop/contact.html', {'form': form, 'submitted': submitted})
