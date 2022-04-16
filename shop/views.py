44dd3ecabca379cc7ef7bd1a88498345103bbadb
dimitarhristov86 <dimitar_hristov86@abv.bg>
2022-04-16 23:59:36 +0300

    add some css

--- shop/views.py
+++ shop/views.py
@@ -8,12 +8,12 @@ from .models import Category, Product
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-id')
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     return render(request, 'shop/product/list.html', {'category': category, 'categories': categories,
-                                                      'products': products})
+                                                      'products': products[:3]})
 
 
 def product_detail(request, id, slug):
