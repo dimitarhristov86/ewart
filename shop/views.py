e368fe0b3ad9720d1e3a77ec45cc2ebab0e12a95
dimitarhristov86 <dimitar_hristov86@abv.bg>
2022-05-19 20:37:39 +0300

    add update profile func

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.mail import send_mail, get_connection
 from django.http import HttpResponseRedirect
-from .forms import ContactForm, UserRegisterForm
+from .forms import ContactForm, UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from .models import Category, Product
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -71,4 +71,20 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+        messages.success(request, 'Your account has been updated! ')
+        return redirect('/profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
